37bd280699c54eaedbd304468799607fd1fc6301
Maksim Surkov <modbrin@gmail.com>
2022-09-21 01:13:58 +0500

    Use macroquad for graphics

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 use device::Chip8;
-use graphics::triangle_demo;
+use graphics::squares_demo;
+use macroquad::window::Conf;
 use std::{env, path::PathBuf, str::FromStr};
 
 mod device;
 mod util;
 mod graphics;
 
-fn main() {
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "CHIP-8 Emulator".to_owned(),
+        fullscreen: false,
+        window_height: 512,
+        window_width: 1024,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
     // let args = env::args().collect::<Vec<_>>();
     // let rom_path_str = args.get(1).expect("Rom path not provided");
     // let rom_path = PathBuf::from_str(&rom_path_str).expect("Malformed rom path");
     // Chip8::new(rom_path).unwrap().run().unwrap();
-    triangle_demo();
+    squares_demo().await;
 }
